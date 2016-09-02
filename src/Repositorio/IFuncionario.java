9cccae6a7a9d455c0b5031f755e483fe5c9ed292
Tayene Carla <tcag18@hotmail.com>
2016-09-02 19:31:05 -0300

    Delete IFuncionario.java

--- src/Repositorio/IFuncionario.java
+++
@@ -1,9 +0,0 @@
-package Repositorio;
-
-import Beans.Funcionario;
-
-public interface IFuncionario extends IRepositorioFuncionario<Funcionario, String> {
-
-	void cadastrar(Funcionario funcionario);
-		
-	}
